127e6024204cff0cc878b2eacbc1b42738fe9f7f
Manuela1908 <154991599+Manuela1908@users.noreply.github.com>
2024-01-22 22:08:47 -0300

    Update receitas.py

--- receitas.py
+++ receitas.py
@@ -1,11 +1,14 @@
+
+from flask import Flask, render_template, request, jsonify
 import requests
 import json
-import tkinter as tk
-from tkinter import scrolledtext
 import os
-from dotenv import load_dotenv
+import dotenv
 
-load_dotenv()
+dotenv.load_dotenv()
+
+app = Flask(__name__, template_folder=r'C:\Users\Manuela.DESKTOP-H5K2INA\Desktop\PROGRAMAS\Python\portfolio\templates')
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
 openai_api_key = os.getenv("OPENAI_API_KEY")
 url_gpt = "https://api.openai.com/v1/chat/completions"
@@ -13,6 +16,19 @@ headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {openai_api_key}"
 }
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/gerar_receita", methods=["POST"])
+def gerar_receita():
+    receita = request.form.get("receita")
+
+    # Obter receita usando o GPT
+    receita_gerada = obter_receita_gpt(receita)
+
+    return jsonify({"receita": receita_gerada})
 
 def obter_receita_gpt(receita):
     data_gpt = {
@@ -22,38 +38,15 @@ def obter_receita_gpt(receita):
             {"role": "user", "content": receita}
         ],
         "temperature": 1,
-        "max_tokens": 400,
+        "max_tokens": 500,
         "top_p": 1,
         "frequency_penalty": 0,
         "presence_penalty": 0
     }
 
     response_gpt = requests.post(url_gpt, headers=headers, data=json.dumps(data_gpt))
-    response_data = response_gpt.json()
+    recipe_result = response_gpt.json()
+    return recipe_result["choices"][0]["message"]["content"]
 
-    # Verifique se 'choices' está presente na resposta
-    if 'choices' in response_data:
-        return response_data["choices"][0]["message"]["content"]
-
-    else:
-        return "Erro na resposta da API: 'choices' não encontrado."
-
-def carregar_receita():
-    receita = entrada_receita.get()
-    texto_receita.config(state=tk.NORMAL)
-    texto_receita.delete(1.0, tk.END)
-    texto_receita.insert(tk.END, obter_receita_gpt(receita))
-    texto_receita.config(state=tk.DISABLED)
-
-janela = tk.Tk()
-janela.title("Geração de Receita")
-
-entrada_receita = tk.Entry(janela, width=50, justify=tk.CENTER)
-botao_gerar = tk.Button(janela, text="Gerar Receita", command=carregar_receita)
-texto_receita = scrolledtext.ScrolledText(janela, width=50, height=20, wrap=tk.WORD)
-
-entrada_receita.pack(pady=10)
-botao_gerar.pack(pady=10)
-texto_receita.pack(pady=10)
-
-janela.mainloop()
+if __name__ == "__main__":
+    app.run(debug=True)
